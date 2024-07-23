1d1e23b999b5984d0c5dff7c08e258649638963e
akash0751 <akashsofficial05@gmail.com>
2024-07-23 18:51:56 +0530

    removed image text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Welcome to Akash web </h1>
-        <h2>This site is to display some text and images</h2>
+        <h2>This site is to display some text</h2>
         
       </header>
       <Navbar />
